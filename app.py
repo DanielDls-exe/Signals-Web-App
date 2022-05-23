c0e6d6794e7993bf1d01d04b3fb992c56369fa27
DanielDls-exe <danieldls.ucv@gmail.com>
2022-05-23 17:36:40 -0400

    signals web app

---
+++ app.py
@@ -0,0 +1,49 @@
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer, VideoProcessorBase
+import cv2 
+import numpy as np 
+from img_process_predict import img_live
+import pickle
+import av
+import threading
+
+with open('SVC_model.pkl', 'rb') as f:
+    svm = pickle.load(f)
+
+st.title("Signals")
+
+class VideoProcessor(VideoProcessorBase):
+        def __init__(self):
+            self.model_lock = threading.Lock()
+
+        def recv(self, frame):
+            img = frame.to_ndarray(format="bgr24")
+            data = img_live(img)
+            data = np.array(data)
+            y_pred = svm.predict(data.reshape(-1,63))
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            position = (50, 100)
+            fontScale = 3
+            color = (0, 0, 0)
+            thickness = 5
+            letter = str(y_pred[0])
+            frame = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            frame = cv2.putText(frame, letter, position, font, 
+                            fontScale, color, thickness, cv2.LINE_AA)
+
+            return av.VideoFrame.from_ndarray(frame)
+
+webrtc_streamer(
+        key="example",
+        video_processor_factory=VideoProcessor,
+        media_stream_constraints={
+            "video": True,
+            "audio": False
+        }
+    )
+
+        
+        
+        
+        
+
